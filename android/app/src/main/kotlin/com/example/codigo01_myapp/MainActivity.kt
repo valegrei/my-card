ab493b40b474612c91f4989aa2e778323027ef6b
valegrei <victoralegre@uni.pe>
2023-01-09 21:51:34 -0500

    Proyecto carta presentacion

---
+++ android/app/src/main/kotlin/com/example/codigo01_myapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codigo01_myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My card App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff51a63c),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50.0,
+            backgroundColor: Colors.orangeAccent,
+            backgroundImage: AssetImage("assets/images/foto1.jpg"),
+          ),
+          Text(
+            "Víctor Augusto Alegre Ibáñez",
+            style: TextStyle(
+                fontSize: 22, color: Colors.white, fontFamily: "Lobster"),
+          ),
+          Text(
+            "FLUTTER DEVELOPER",
+            style: TextStyle(
+                fontSize: 20, color: Colors.white70, fontFamily: "Manrope"),
+          ),
+          Divider(
+            thickness: 0.9,
+            indent: 70,
+            endIndent: 70,
+            color: Colors.white,
+          ),
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: Color(0xff51a63c),
+                size: 30.0,
+              ),
+              title: Text(
+                "+51 924 448 546",
+                style: TextStyle(fontFamily: "Manrope", fontSize: 18.0),
+              ),
+            ),
+          ),
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.email,
+                color: Color(0xff51a63c),
+                size: 30.0,
+              ),
+              title: Text(
+                "valegreib@gmail.com",
+                style: TextStyle(fontFamily: "Manrope", fontSize: 18.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 24.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/facebook.png",
+                height: 42.0,
+              ),
+              SizedBox(width: 24.0),
+              Image.asset(
+                "assets/images/twitter.png",
+                height: 42.0,
+              ),
+              SizedBox(width: 24.0),
+              Image.asset(
+                "assets/images/instagram.png",
+                height: 42.0,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
